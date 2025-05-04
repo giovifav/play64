381e7358170edd7f74d47901d0e564f2b29d7e98
Giovanni Favaro <wip.gio@gmail.com>
2025-05-04 20:32:44 +0200

    Rimuove .bmx dal tracking e la aggiunge a .gitignore

--- .bmx/list.bmx.gui.release.win32.x64.i
+++
@@ -1,3 +0,0 @@
-superstrict
-import brl.blitz
-import ray.lib
--- .bmx/matrice.bmx.release.linux.x64.incbin2.c
+++
@@ -1,6 +0,0 @@
-#define INCBIN_PREFIX _ib
-#define INCBIN_STYLE INCBIN_STYLE_SNAKE
-#include "brl.mod/blitz.mod/incbin/incbin.h"
-// FILE : "lualibs.lua"	a32f8c86507e2f4e
-// ----
-INCBIN(_bb_main_1, "/home/jo/Scrivania/matrice.bmx/lualibs.lua");
